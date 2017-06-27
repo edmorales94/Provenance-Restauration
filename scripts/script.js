5814eaca0509f2f1c81d70fd06b302f87b8c04d9
Edgar Morales <edmorales94@gmail.com>
2017-06-27 15:25:09 -0400

    Photos rotate with a click

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,26 @@ $(document).ready(function () {
         interval: 5000
     })
 });
+
+$('.f1_container').click(function() {
+    $(this).toggleClass('active');
+});
+
+$('.flip3D').click(function() {
+    $(this).toggleClass('active');
+});
+
+(function() {
+    var cards = document.querySelectorAll(".card.effect__click");
+    for ( var i  = 0, len = cards.length; i < len; i++ ) {
+        var card = cards[i];
+        clickListener( card );
+    }
+
+    function clickListener(card) {
+        card.addEventListener( "click", function() {
+            var c = this.classList;
+            c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+        });
+    }
+})();
